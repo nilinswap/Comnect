f70ce1fb1dd97f12833662128ddbb364649bbd94
placements2017 <nilinswap@gmail.com>
2018-08-14 11:17:37 +0530

    now client is consistent with readme

--- client.py
+++ client.py
@@ -3,10 +3,10 @@ import time
 import sys
 s = socket.socket()
 
-port = int(sys.argv[1])
+port = int(sys.argv[1]) #2020 for server being behind router( mine)
 
-s.connect(("172.20.51.18", port))
-
+s.connect(("172.40.71.15", port)) 
+'''this is router's Ip. Further the 'port' is not the port of this process; it is the port at which server is to be requested. The port of this process (which is printed in first line of server.py) is arbitrary.'''
 while True:
 	st = s.recv(1024)
 	print(st)
