600622ee3969790cc34fe543047f3a4c75e87b21
nilinswap <nilinswap@gmail.com>
2018-08-15 13:19:57 +0530

    commit of the branch

--- client.py
+++ client.py
@@ -1,17 +1,11 @@
 import socket
-import time
-import sys
-s = socket.socket()
+import sys, datetime
 
-port = int(sys.argv[1]) #2020 for server being behind router( mine)
+udp_ip_address = "xxx"
+udp_port_no = 3030
+message = "hello, server"
 
-s.connect(("172.40.71.15", port)) 
-'''this is router's Ip. Further the 'port' is not the port of this process; it is the port at which server is to be requested. The port of this process (which is printed in first line of server.py) is arbitrary.'''
-while True:
-	st = s.recv(1024)
-	print(st)
-	#s.send("ok then".encode())
-	time.sleep(5)
-s.close()
+clientsock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
+clientsock.send(message.encode(), (udp_id_address, udp_port_no))
 
--- server.py
+++ server.py
@@ -1,25 +1,12 @@
 import socket
-import datetime
-import sys
-import time
-s = socket.socket()
-print("socket successfully created")
-port = int(sys.argv[1])
-s.bind(("",port))
-s.listen(5)
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-c, addr = s.accept()
-print("Got connection from ", addr)
+
+udp_ip_address = "xxx"
+udp_port_no = 2020
+
+serversock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+serversock.bind((udp_ip_address, udp_port_no))
+
 while True:
-	#c, addr = s.accept()
-	#print("Got connection from", addr)
-	#print(addr, "sent: ", c.recv(1024))
-	st = (str(datetime.datetime.now())+"\n").encode()
-	c.send(st)
-	#print(addr, "sent: ", c.recv(1024))
-	st = "Thank you:)\n".encode()
-	#print(addr, "sent: ", c.recv(1024))
-	c.send(st)
-	time.sleep(2)
-c.close()
-s.close()
+	data, addr = serversock.recvfrom(1024)
+	print( "message", data)
