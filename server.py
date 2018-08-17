5ebf6a1d1e93180847d7c7d2599d7c279f254948
nilinswap <nilinswap@gmail.com>
2018-08-17 14:24:06 +0530

    server for udp broadcast

--- server.py
+++ server.py
@@ -1,42 +1,15 @@
-import socket
-import threading
-import sys
-BUF_SIZ = 1024
-reply_dic = {'black':'white', 'yin':'yang', 'sun':'earth', 'batman': 'joker' }
+# Send UDP broadcast packets
 
-def handle_client(cli_s, cli_addr):
-	#cli_s = arg_tup[0]
-	st ='first'
-	while st:
-		st = cli_s.recv(BUF_SIZ)
-		st = st.decode()
-		print(cli_addr, " requested for ", st)
-		if not st:
-			print("connection broken with ", cli_addr)
-			return 1
-		if st not in reply_dic:
-			rep_st = "stupid request"
-		else:
-			rep_st = reply_dic[st]
-		print("\t\t\t sending reply to %s : %s  "%(cli_addr, rep_st))
-		rep_st = rep_st.encode()
-		cli_s.send(rep_st)
-	cli_s.close()
-	return 0
+MYPORT = 50000
 
-request_id = 1
-serv_s = socket.socket( socket.AF_INET, socket.SOCK_STREAM)
-print("socket successfully created")
-port = 2020
-serv_s.bind(("",port))
+import sys, time
+from socket import *
 
-serv_s.listen(5)
-thread_lis = []
-while True:
-	cli_s, addr = serv_s.accept()
-	print("\t\t\tGot connection from ", addr)
-	thr = threading.Thread(target = handle_client, args = (cli_s, addr))
-	thr.start()
-	thread_lis.append(thr)
+s = socket(AF_INET, SOCK_DGRAM)
+s.bind(('', 0))
+s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
 
-serv_s.close()
+while 1:
+    data = repr(time.time()) + '\n'
+    s.sendto(data.encode(), ('<broadcast>', MYPORT))
+    time.sleep(2)
